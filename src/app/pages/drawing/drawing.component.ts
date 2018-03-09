ae855d57ad3c49e53d0ac5d88a468b708e366910
Houdou <houdou.pt@gmail.com>
2018-03-10 04:50:55 +0800

    Beat board

--- src/app/pages/drawing/drawing.component.ts
+++ src/app/pages/drawing/drawing.component.ts
@@ -26,6 +26,14 @@ export class FueDrawingComponent implements OnInit {
 		this.stage.scaleX = this.stage.scaleY = v;
 	}
 
+	private _pixelScale : number;
+	public get pixelScale() : number {
+		return this._pixelScale;
+	}
+	public set pixelScale(v : number) {
+		this._pixelScale = v;
+	}
+
 	constructor(private ele: ElementRef) { }
 
 	ngOnInit() {
@@ -39,11 +47,13 @@ export class FueDrawingComponent implements OnInit {
 	private seeds: Array<createjs.Shape> = new Array<createjs.Shape>();
 
 	@HostListener('window:resize')
-	onResize() {    	
-		this.canvas.height = this.canvas.offsetHeight;
-		this.canvas.width = this.canvas.offsetWidth;
+	onResize() {
+		let smallSize = Math.min(this.canvas.parentElement.offsetHeight, this.canvas.parentElement.offsetWidth) + 80;
+		this.pixelScale = Math.floor(Math.log2(smallSize));
+		this.canvas.width = this.canvas.height = 2**this.pixelScale + 80;
 		this.stage.x = this.canvas.width / 2;
 		this.stage.y = this.canvas.height / 2;
+		this.stage.scaleX = this.stage.scaleY = 2**(this.pixelScale-8)+.5;
 
 		this.progressBar.graphics.clear().beginFill('#D0D0D0').dr(0, 1, this.canvas.width, 3);
 
@@ -54,7 +64,7 @@ export class FueDrawingComponent implements OnInit {
 				this.scale = Math.min(sW, sH, 1);
 			}
 		}
-
+		this.canvas.getContext('2d').imageSmoothingEnabled = false;
 		this.stage.update();
 	}
 
@@ -65,25 +75,54 @@ export class FueDrawingComponent implements OnInit {
 	}
 
 	private setupCanvas(): void {
-		this.canvas.height = this.canvas.offsetHeight;
-		this.canvas.width = this.canvas.offsetWidth;
 		this.stage = new createjs.Stage(this.canvas);
 		this.stage.autoClear = true;
+		this.stage.snapToPixelEnabled = true;
+		this.stage.snapToPixel = false;
 		this.stage.enableMouseOver();
 		this.uiContainer = new createjs.Container();
-
 		this.progressBar = new createjs.Shape();
-		this.progressBar.graphics.beginFill('#D0D0D0').dr(0, 1, this.canvas.width, 3);
 		this.progressBar.scaleX = 0;
 		this.uiContainer.addChild(this.progressBar);
-
 		this.stage.addChild(this.uiContainer);
 		this.stage.setChildIndex(this.uiContainer, this.stage.numChildren);
-		this.stage.update();
+
+		this.onResize();
 	}
 
 	private createBeatMesh(length: number = 16): void {
-		let c = new createjs.Container();
+		let padding = 3;
+		let spacing = 2;
+		let cellSize = 8;
+		let cellRadius = 2.5;
 
+		let c = new createjs.Container();
+		let bg = new createjs.Shape();
+		let bgSize = (cellSize + spacing) * length - spacing + 2 * padding;
+		bg.graphics.beginFill('#666666').drawRoundRect(0, 0, bgSize, bgSize, 1.5);
+		c.addChild(bg);
+
+		for(let v = 0; v < length; v++) {
+			for(let u = 0; u < length; u++) {
+				let cell = new createjs.Shape();
+				// TODO: Create model
+				// TOOD: Bind events
+				if(Math.random() < 0.8)
+					cell.graphics.f('#888888').drawRoundRectComplex(0, 0, cellSize, cellSize, cellRadius, 0, 0, 0);
+				else
+					cell.graphics.f('#FBEF98').drawRoundRectComplex(0, 0, cellSize, cellSize, cellRadius, 0, 0, 0);
+				cell.x = padding + (cellSize + spacing) * u;
+				cell.y = padding + (cellSize + spacing) * v;
+				c.addChild(cell);
+			}
+		}
+		c.cache(0, 0, bgSize, bgSize);
+		c.regX = c.regY = bgSize / 2;
+		this.stage.scaleX = this.stage.scaleY = 2**(this.pixelScale-8)+.5;
+
+		this.stage.addChild(c);
+		setTimeout(() => {
+			this.stage.update();
+		});
 	}
 }
